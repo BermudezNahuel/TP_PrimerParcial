72118ee1eca77e32dbaf0658f519ebf421057c90
Nahuel <Nahuel@DESKTOP-3BU0DCU>
2023-04-24 21:32:16 -0300

    Agregar archivos de proyecto.

---
+++ PrimerParcial/Program.cs
@@ -0,0 +1,13 @@
+﻿using PrimerParcialBiblioteca;
+
+namespace PrimerParcial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Persona sujetoUno = new Persona("Nahuel", "Bermudez");
+        }
+    }
+}
---
+++ PrimerParcialBiblioteca/Persona.cs
@@ -0,0 +1,24 @@
+﻿namespace PrimerParcialBiblioteca
+{
+    public class Persona
+
+    {
+        private string _nombre;
+        private string _apellido;
+        private string _dni;
+        private string _contraseña;
+
+        public Persona(string nombre, string apellido, string dni)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _dni = dni;
+            _contraseña = dni;
+        }
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public string Dni { get => _dni; set => _dni = value; }
+        public string Contraseña { get => _contraseña; set => _contraseña = value; }
+    }
+}
